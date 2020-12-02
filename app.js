37bbf4b6fcfeb9a61435d091f3246373f126246a
Thomas Olsson <olss.thom@gmail.com>
2020-12-02 11:01:28 +0100

    Grund

---
+++ app.js
@@ -0,0 +1,15 @@
+function Produkt (name, description, price){
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.addToCart = function() {
+    }
+    this.edit = function() {
+    }
+    this.delete = function() {
+    }
+    this.image = function() {
+    }
+}
+
+let Tennisracket = new Produkt ("Tennisracket", "Används för att spela tennis", 200);

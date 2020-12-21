cddabee57fa2fd3c72b0699f967626039b6a95bb
Andrés <andres.santana@medieinstitutet.se>
2020-12-21 15:14:19 +0100

    fixed input

--- checkout.js
+++ checkout.js
@@ -1,13 +1,10 @@
 const name = document.getElementById('fnamn');
 const lastname = document.getElementById('lnamn');
-
-
-
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if(name.value === '' || name.value == null){
+    if(fnamn.value === '' || fnamn.value == null) {
         messages.push('Fyll i namn')
     }
     e.preventDefault()

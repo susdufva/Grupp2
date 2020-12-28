caa0f7d7bfe21a8d16a08fe39b93d55fa28afd45
Malin Sjoblom <malin.sjoblom@hotmail.com>
2020-12-28 13:27:41 +0100

    checkout function

--- contact-us.js
+++ contact-us.js
@@ -1,5 +1,5 @@
 
-// 
+// Förnamn + Efternamn måste vara ifyllt för att formuläret ska kunna skickas
 function validateForm () {
     let contactForm = document.forms["myForm"] ["name"].value
     if (contactForm == null || contactForm == "") {
